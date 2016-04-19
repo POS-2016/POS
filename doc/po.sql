19370021e7210dd3b09f92bcaaaa034b12b685de
RainWhite <396624926@qq.com>
2016-04-19 11:17:06 +0800

    数据库表

--- doc/po.sql
+++ doc/po.sql
@@ -10,97 +10,40 @@ Target Server Type    : MYSQL
 Target Server Version : 50140
 File Encoding         : 65001
 
-Date: 2016-03-29 15:21:27
+Date: 2016-04-19 11:16:07
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for `parking_lot`
--- ----------------------------
-DROP TABLE IF EXISTS `parking_lot`;
-CREATE TABLE `parking_lot` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `status` varchar(255) NOT NULL DEFAULT '可用',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of parking_lot
--- ----------------------------
-
--- ----------------------------
--- Table structure for `parking_space`
+-- Table structure for parking_space
 -- ----------------------------
 DROP TABLE IF EXISTS `parking_space`;
 CREATE TABLE `parking_space` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `space_number` int(5) NOT NULL,
-  `car_number` varchar(20) NOT NULL,
-  `status` varchar(2) NOT NULL,
-  `start_time` datetime NOT NULL,
-  `end_time` datetime NOT NULL,
-  `cost` double(10,2) NOT NULL,
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `status` varchar(255) NOT NULL DEFAULT '可用',
+  `pp_name` bigint(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=603 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of parking_space
 -- ----------------------------
-INSERT INTO `parking_space` VALUES ('1', '-1', '冀H1', '空', '2016-03-17 11:48:36', '2016-03-17 11:48:40', '70.00');
-INSERT INTO `parking_space` VALUES ('553', '0', '京A0', '满', '2016-03-18 10:45:59', '2016-03-18 10:45:59', '70.00');
-INSERT INTO `parking_space` VALUES ('554', '1', '京A1', '满', '2016-03-16 10:46:02', '2016-03-16 10:46:02', '50.00');
-INSERT INTO `parking_space` VALUES ('555', '2', '京A2', '满', '2016-03-16 10:46:02', '2016-03-16 10:46:02', '50.00');
-INSERT INTO `parking_space` VALUES ('556', '3', '京A3', '满', '2016-03-16 10:46:02', '2016-03-16 10:46:02', '50.00');
-INSERT INTO `parking_space` VALUES ('557', '4', '京A4', '满', '2016-03-16 10:46:02', '2016-03-16 10:46:02', '50.00');
-INSERT INTO `parking_space` VALUES ('558', '5', '京A5', '满', '2016-03-16 10:46:02', '2016-03-16 10:46:02', '50.00');
-INSERT INTO `parking_space` VALUES ('559', '6', '京A6', '满', '2016-03-16 10:46:02', '2016-03-16 10:46:02', '50.00');
-INSERT INTO `parking_space` VALUES ('560', '7', '京A7', '满', '2016-03-16 10:46:02', '2016-03-16 10:46:02', '50.00');
-INSERT INTO `parking_space` VALUES ('561', '8', '京A8', '满', '2016-03-16 10:46:02', '2016-03-16 10:46:02', '50.00');
-INSERT INTO `parking_space` VALUES ('562', '9', '京A9', '满', '2016-03-16 10:46:02', '2016-03-16 10:46:02', '50.00');
-INSERT INTO `parking_space` VALUES ('563', '10', '京A10', '满', '2016-03-16 10:46:02', '2016-03-16 10:46:02', '50.00');
-INSERT INTO `parking_space` VALUES ('564', '11', '京A11', '满', '2016-03-16 10:46:02', '2016-03-16 10:46:02', '50.00');
-INSERT INTO `parking_space` VALUES ('565', '12', '京A12', '满', '2016-03-16 10:46:02', '2016-03-16 10:46:02', '50.00');
-INSERT INTO `parking_space` VALUES ('566', '13', '京A13', '满', '2016-03-16 10:46:02', '2016-03-16 10:46:02', '50.00');
-INSERT INTO `parking_space` VALUES ('567', '14', '京A14', '满', '2016-03-16 10:46:02', '2016-03-16 10:46:02', '50.00');
-INSERT INTO `parking_space` VALUES ('568', '15', '京A15', '满', '2016-03-16 10:46:02', '2016-03-16 10:46:02', '50.00');
-INSERT INTO `parking_space` VALUES ('569', '16', '京A16', '满', '2016-03-16 10:46:02', '2016-03-16 10:46:02', '50.00');
-INSERT INTO `parking_space` VALUES ('570', '17', '京A17', '满', '2016-03-16 10:46:02', '2016-03-16 10:46:02', '50.00');
-INSERT INTO `parking_space` VALUES ('571', '18', '京A18', '满', '2016-03-16 10:46:02', '2016-03-16 10:46:02', '50.00');
-INSERT INTO `parking_space` VALUES ('572', '19', '京A19', '满', '2016-03-16 10:46:02', '2016-03-16 10:46:02', '50.00');
-INSERT INTO `parking_space` VALUES ('573', '20', '京A20', '满', '2016-03-16 10:46:02', '2016-03-16 10:46:02', '50.00');
-INSERT INTO `parking_space` VALUES ('574', '21', '京A21', '满', '2016-03-16 10:46:02', '2016-03-16 10:46:02', '50.00');
-INSERT INTO `parking_space` VALUES ('575', '22', '京A22', '满', '2016-03-16 10:46:02', '2016-03-16 10:46:02', '50.00');
-INSERT INTO `parking_space` VALUES ('576', '23', '京A23', '满', '2016-03-16 10:46:02', '2016-03-16 10:46:02', '50.00');
-INSERT INTO `parking_space` VALUES ('577', '24', '京A24', '满', '2016-03-16 10:46:03', '2016-03-16 10:46:03', '50.00');
-INSERT INTO `parking_space` VALUES ('578', '25', '京A25', '满', '2016-03-16 10:46:03', '2016-03-16 10:46:03', '50.00');
-INSERT INTO `parking_space` VALUES ('579', '26', '京A26', '满', '2016-03-16 10:46:03', '2016-03-16 10:46:03', '50.00');
-INSERT INTO `parking_space` VALUES ('580', '27', '京A27', '满', '2016-03-16 10:46:03', '2016-03-16 10:46:03', '50.00');
-INSERT INTO `parking_space` VALUES ('581', '28', '京A28', '满', '2016-03-16 10:46:03', '2016-03-16 10:46:03', '50.00');
-INSERT INTO `parking_space` VALUES ('582', '29', '京A29', '满', '2016-03-16 10:46:03', '2016-03-16 10:46:03', '50.00');
-INSERT INTO `parking_space` VALUES ('583', '30', '京A30', '满', '2016-03-16 10:46:03', '2016-03-16 10:46:03', '50.00');
-INSERT INTO `parking_space` VALUES ('584', '31', '京A31', '满', '2016-03-16 10:46:03', '2016-03-16 10:46:03', '50.00');
-INSERT INTO `parking_space` VALUES ('585', '32', '京A32', '满', '2016-03-16 10:46:03', '2016-03-16 10:46:03', '50.00');
-INSERT INTO `parking_space` VALUES ('586', '33', '京A33', '满', '2016-03-16 10:46:03', '2016-03-16 10:46:03', '50.00');
-INSERT INTO `parking_space` VALUES ('587', '34', '京A34', '满', '2016-03-16 10:46:03', '2016-03-16 10:46:03', '50.00');
-INSERT INTO `parking_space` VALUES ('588', '35', '京A35', '满', '2016-03-16 10:46:03', '2016-03-16 10:46:03', '50.00');
-INSERT INTO `parking_space` VALUES ('589', '36', '京A36', '满', '2016-03-16 10:46:03', '2016-03-16 10:46:03', '50.00');
-INSERT INTO `parking_space` VALUES ('590', '37', '京A37', '满', '2016-03-16 10:46:03', '2016-03-16 10:46:03', '50.00');
-INSERT INTO `parking_space` VALUES ('591', '38', '京A38', '满', '2016-03-16 10:46:03', '2016-03-16 10:46:03', '50.00');
-INSERT INTO `parking_space` VALUES ('592', '39', '京A39', '满', '2016-03-16 10:46:03', '2016-03-16 10:46:03', '50.00');
-INSERT INTO `parking_space` VALUES ('593', '40', '京A40', '满', '2016-03-16 10:46:03', '2016-03-16 10:46:03', '50.00');
-INSERT INTO `parking_space` VALUES ('594', '41', '京A41', '满', '2016-03-16 10:46:03', '2016-03-16 10:46:03', '50.00');
-INSERT INTO `parking_space` VALUES ('595', '42', '京A42', '满', '2016-03-16 10:46:03', '2016-03-16 10:46:03', '50.00');
-INSERT INTO `parking_space` VALUES ('596', '43', '京A43', '满', '2016-03-16 10:46:03', '2016-03-16 10:46:03', '50.00');
-INSERT INTO `parking_space` VALUES ('597', '44', '京A44', '满', '2016-03-16 10:46:03', '2016-03-16 10:46:03', '50.00');
-INSERT INTO `parking_space` VALUES ('598', '45', '京A45', '满', '2016-03-16 10:46:03', '2016-03-16 10:46:03', '50.00');
-INSERT INTO `parking_space` VALUES ('599', '46', '京A46', '满', '2016-03-16 10:46:03', '2016-03-16 10:46:03', '50.00');
-INSERT INTO `parking_space` VALUES ('600', '47', '京A47', '满', '2016-03-16 10:46:03', '2016-03-16 10:46:03', '50.00');
-INSERT INTO `parking_space` VALUES ('601', '48', '京A48', '满', '2016-03-16 10:46:03', '2016-03-16 10:46:03', '50.00');
-INSERT INTO `parking_space` VALUES ('602', '49', '京A49', '满', '2016-03-16 10:46:03', '2016-03-16 10:46:03', '50.00');
+INSERT INTO `parking_space` VALUES ('1', '正在使用', '1');
+INSERT INTO `parking_space` VALUES ('2', '已预订', '2');
+INSERT INTO `parking_space` VALUES ('3', '已预订', '3');
+INSERT INTO `parking_space` VALUES ('4', '已预订', '4');
+INSERT INTO `parking_space` VALUES ('5', '可用', '5');
+INSERT INTO `parking_space` VALUES ('6', '可用', '6');
+INSERT INTO `parking_space` VALUES ('7', '可用', '7');
+INSERT INTO `parking_space` VALUES ('8', '可用', '8');
+INSERT INTO `parking_space` VALUES ('9', '正在使用', '9');
+INSERT INTO `parking_space` VALUES ('10', '已预定', '10');
+INSERT INTO `parking_space` VALUES ('17', '已预订', '11');
+INSERT INTO `parking_space` VALUES ('18', '可用', '12');
 
 -- ----------------------------
--- Table structure for `person_info`
+-- Table structure for person_info
 -- ----------------------------
 DROP TABLE IF EXISTS `person_info`;
 CREATE TABLE `person_info` (
@@ -112,35 +55,51 @@ CREATE TABLE `person_info` (
   `avatar` varchar(255) NOT NULL,
   `token` varchar(32) NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of person_info
 -- ----------------------------
+INSERT INTO `person_info` VALUES ('1', 'username_1', 'password', '18810409001', '女', '21221331', '14594071369770000');
+INSERT INTO `person_info` VALUES ('2', 'username_2', 'password', '18810409002', '男', '21221331', '14594071370430000');
+INSERT INTO `person_info` VALUES ('3', 'username_3', 'password', '18810409003', '女', '21221331', '14594071370650000');
+INSERT INTO `person_info` VALUES ('4', 'username_4', 'password', '18810409004', '男', '21221331', '14594071370790000');
+INSERT INTO `person_info` VALUES ('5', 'username_5', 'password', '18810409005', '女', '21221331', '14594071370920000');
+INSERT INTO `person_info` VALUES ('6', 'username_6', 'password', '18810409006', '男', '21221331', '14594071371120000');
+INSERT INTO `person_info` VALUES ('7', 'username_7', 'password', '18810409007', '女', '21221331', '14594071371330000');
+INSERT INTO `person_info` VALUES ('8', 'username_8', 'password', '18810409008', '男', '21221331', '14594071371550000');
+INSERT INTO `person_info` VALUES ('9', 'username_9', 'password', '18810409009', '女', '21221331', '14594071371730000');
+INSERT INTO `person_info` VALUES ('10', 'username_10', 'password', '18810409001', '男', '21221331', '14594071371890000');
+INSERT INTO `person_info` VALUES ('11', 'bbb', '25d55ad283aa400af464c76d713c07ad', '11111111111', '', 'http://pic.xoyo.com/bbs/2011/05/05/1105052118830c2d625c0efe2e.jpg', '');
 
 -- ----------------------------
--- Table structure for `reserve`
+-- Table structure for reserve
 -- ----------------------------
 DROP TABLE IF EXISTS `reserve`;
 CREATE TABLE `reserve` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `order_time` datetime NOT NULL,
+  `order_time` bigint(14) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `space_id` int(11) NOT NULL,
   `fee` double(5,2) NOT NULL,
+  `status` int(1) NOT NULL DEFAULT '0',
+  `space_id` bigint(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
-  KEY `space_id` (`space_id`),
-  CONSTRAINT `reserve_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `person_info` (`user_id`),
-  CONSTRAINT `reserve_ibfk_2` FOREIGN KEY (`space_id`) REFERENCES `parking_lot` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  KEY `reserve_ibfk_2` (`space_id`),
+  CONSTRAINT `reserve_ibfk_2` FOREIGN KEY (`space_id`) REFERENCES `parking_space` (`id`),
+  CONSTRAINT `reserve_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `person_info` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of reserve
 -- ----------------------------
+INSERT INTO `reserve` VALUES ('37', '1460974545136', '11', '3.00', '1', '5');
+INSERT INTO `reserve` VALUES ('38', '1460974615080', '11', '3.00', '1', '6');
+INSERT INTO `reserve` VALUES ('39', '1460977229009', '11', '3.00', '1', '6');
+INSERT INTO `reserve` VALUES ('40', '1460977783401', '11', '3.00', '0', '7');
 
 -- ----------------------------
--- Table structure for `servant`
+-- Table structure for servant
 -- ----------------------------
 DROP TABLE IF EXISTS `servant`;
 CREATE TABLE `servant` (
@@ -148,11 +107,12 @@ CREATE TABLE `servant` (
   `password` varchar(40) NOT NULL,
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `status` int(1) NOT NULL,
+  `authority` int(10) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of servant
 -- ----------------------------
-INSERT INTO `servant` VALUES ('admin', '1966dd715c6d14f36578b6ab9fb0896d', '1', '1');
-INSERT INTO `servant` VALUES ('guest', '9a6dd60eb24928c2d83835eece96c479', '2', '1');
+INSERT INTO `servant` VALUES ('admin', '1966dd715c6d14f36578b6ab9fb0896d', '1', '1', '0');
+INSERT INTO `servant` VALUES ('guest', '9a6dd60eb24928c2d83835eece96c479', '2', '1', '1');
